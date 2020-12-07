0c5dfbe25a368b29b867da562d02ef93b8904976
yesol0502 <6.6yesol@gmail.com>
2020-12-07 23:14:52 +0900

    db연결

--- bungsujili/src/main/java/com/kh/bungsu/BungsujiliApplication.java
+++ bungsujili/src/main/java/com/kh/bungsu/BungsujiliApplication.java
@@ -2,8 +2,10 @@ package com.kh.bungsu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = {"classpath:jdbc.properties"})
 public class BungsujiliApplication {
 
 	public static void main(String[] args) {
